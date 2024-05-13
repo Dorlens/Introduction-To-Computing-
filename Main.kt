8d1282f031a86633869455f2ab34db6acaf0e64d
Dorlens <160911732+Dorlens@users.noreply.github.com>
2024-05-13 05:52:38 -0400

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,10 +1,24 @@
+//Ex 3: reimplement Harmonic sequence ( please refer to page 65 of our textbook). The number of elements of the sequence should come from the user input.
 
-//Ex 4: reimplement a random sequence numbers 
+
+
 
 fun main(args: Array<String>)
 {
-         // Print a random sequence of n real values in [0, 1)
-            val n = args[0].toInt()
-            for (i in 0 until n) println(Math.random())
-        }
+    print("Enter a  number ")
+    val userInput= readlnOrNull()
+    val  n = userInput?.toIntOrNull()
 
+
+    if (n != null)
+    {
+        var sum = 0.0
+        for (i in 1..n) {
+
+            sum += 1.0 / i
+        }
+        println(sum)
+    }
+    else
+        print("Enter a  valid number ")
+    }
