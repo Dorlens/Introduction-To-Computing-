c131d10efb71da7e0f8b050337dc80c9cde06acf
Dorlens <160911732+Dorlens@users.noreply.github.com>
2024-05-13 06:01:12 -0400

    Add files via upload

---
+++ Chapter1Exercises4.kt
@@ -0,0 +1,10 @@
+
+
+
+fun main(args: Array<String>)
+{
+         // Print a random sequence of n real values in [0, 1)
+            val n = args[0].toInt()
+            for (i in 0 until n) println(Math.random())
+        }
+
